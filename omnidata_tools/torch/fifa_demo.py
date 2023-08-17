e7e3df6f3879158708fb31dd309909fadc6780f3
ZetongZhangZ <71453083+ZetongZhangZ@users.noreply.github.com>
2023-08-17 17:03:09 +0200

    update requirements.txt

--- omnidata_tools/torch/fifa_demo.py
+++ omnidata_tools/torch/fifa_demo.py
@@ -42,42 +42,43 @@ def setup_normal_model(ckpt_dir, device):
     model.to(device)
     return model
 
+
 def get_omnidata_depth(model, images, device, output_dir):
-    image_size = (384,384)
+    image_size = (384, 384)
     trans_totensor = transforms.Compose([transforms.Resize(image_size, interpolation=PIL.Image.BILINEAR),
-                                        transforms.ToTensor(),
+                                         transforms.ToTensor(),
                                          transforms.Normalize(mean=0.5, std=0.5)])
     depth_list = []
-    for i,image in enumerate(images):
-        w,h = image.size
+    for i, image in enumerate(images):
+        w, h = image.size
         img_tensor = trans_totensor(image)[:3].unsqueeze(0).to(device)
         output = model(img_tensor).clamp(min=0, max=1)
-        output = F.interpolate(output.unsqueeze(0),(h,w) , mode='bicubic').squeeze(0)
+        output = F.interpolate(output.unsqueeze(0), (h, w), mode='bicubic').squeeze(0)
         output = output.clamp(0, 1)
         output_numpy = output.detach().cpu().numpy().squeeze()
 
-        imgpath = os.path.join(output_dir,f"frame_{str(i).zfill(6)}.png")
-        npypath = os.path.join(output_dir,f"frame_{str(i).zfill(6)}.npy")
-        plt.imsave(imgpath,output_numpy,cmap='viridis')
-        np.save(npypath,output_numpy)
-
+        imgpath = os.path.join(output_dir, f"frame_{str(i).zfill(6)}.png")
+        npypath = os.path.join(output_dir, f"frame_{str(i).zfill(6)}.npy")
+        plt.imsave(imgpath, output_numpy, cmap='viridis')
+        np.save(npypath, output_numpy)
 
         depth_list.append(output_numpy)
     return depth_list
 
-def get_omnidata_normal(model,images,device,output_dir):
-    image_size = (384,384)
+
+def get_omnidata_normal(model, images, device, output_dir):
+    image_size = (384, 384)
     trans_totensor = transforms.Compose([transforms.Resize(image_size, interpolation=PIL.Image.BILINEAR),
                                          get_transform('rgb', image_size=None)])
 
     normal_list = []
-    for i,image in enumerate(images):
+    for i, image in enumerate(images):
         w, h = image.size
         img_tensor = trans_totensor(image)[:3].unsqueeze(0).to(device)
         output = model(img_tensor).clamp(min=0, max=1)
-        output = F.interpolate(output, (h,w), mode='bicubic')
+        output = F.interpolate(output, (h, w), mode='bicubic')
         output = output.clamp(0, 1)
-        output_normalized = (2 * output - 1).permute(0,2,3,1)
+        output_normalized = (2 * output - 1).permute(0, 2, 3, 1)
         output_normalized = torch.nn.functional.normalize(output_normalized, p=2, dim=-1)
         output_normalized = output_normalized.detach().cpu().numpy()[0]
 
@@ -107,13 +108,13 @@ if __name__ == '__main__':
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     ckpt_dir = "pretrained_models"
 
-    depth_model = setup_depth_model(ckpt_dir,device)
+    depth_model = setup_depth_model(ckpt_dir, device)
     normal_model = setup_normal_model(ckpt_dir, device)
 
     output_dir = '/home/fspinola/zetong/fifa/regnerf/data/fifa_TUN-ALG/'
     depth_dir = output_dir + f'omnidepths_{factor}'
     normal_dir = output_dir + f'omninormals_{factor}'
-    os.makedirs(depth_dir,exist_ok=True)
-    os.makedirs(normal_dir,exist_ok=True)
-    get_omnidata_depth(depth_model,images,device,depth_dir)
-    get_omnidata_normal(normal_model,images,device,normal_dir)
+    os.makedirs(depth_dir, exist_ok=True)
+    os.makedirs(normal_dir, exist_ok=True)
+    get_omnidata_depth(depth_model, images, device, depth_dir)
+    get_omnidata_normal(normal_model, images, device, normal_dir)
